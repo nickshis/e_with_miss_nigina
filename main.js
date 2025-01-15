0074d2d856f9feccf9fca7d96e8b9b4330b0e5a9
nickshis <137713260+nickshis@users.noreply.github.com>
2025-01-15 22:35:51 +0500

    skibidi

--- main.js
+++ main.js
@@ -10,5 +10,23 @@ document.addEventListener("DOMContentLoaded", function () {
       mobileNav.style.display = mobileNav.classList.contains("active")
         ? "flex"
         : "none";
-    });
   });
+});
+
+const carousel = document.querySelector('.reviews-carousel');
+
+    function startCarousel() {
+      const reviews = carousel.querySelectorAll('.review');
+      const firstReview = reviews[0];
+
+      carousel.style.transition = 'transform 0.5s ease';
+      carousel.style.transform = `translateX(-${firstReview.offsetWidth + 20}px)`;
+
+      setTimeout(() => {
+        carousel.style.transition = 'none';
+        carousel.style.transform = 'translateX(0)';
+        carousel.appendChild(firstReview);
+      }, 500);
+    }
+
+    setInterval(startCarousel, 3000);
